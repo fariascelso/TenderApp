324599ab9c1217ca8ffee31d817a79d22f6d9565
Celso Farias <c__farias@outlook.com>
2025-02-09 12:00:00 -0300

    fix: apply mask to phone + zipcode + cnpj

--- scripts/main.js
+++ scripts/main.js
@@ -623,8 +623,8 @@ function applyInputMasks(container) {
     phoneInputs.forEach(input => {
         IMask(input, {
             mask: '(00) 00000-0000'
-        })
-    })
+        });
+    });
 
     const moneyInputs = container.querySelectorAll('.amountService, .unitPriceEquipment');
     moneyInputs.forEach(input => {
@@ -633,28 +633,29 @@ function applyInputMasks(container) {
             blocks: {
                 num: {
                     mask: Number,
-                    thousandsSeparator: '.',
+                    thousandsSeparator: '.', 
                     radix: ',',
                     mapToRadix: ['.'],
                     scale: 2,
                     signed: false,
-                    normalizeZeros: false
+                    padFractionalZeros: true,
+                    normalizeZeros: true
                 }
             }
-        })
-    })
+        });
+    });
 
     const cnpjInputs = container.querySelectorAll('#cpfCnpj, #cpfCNPJClient');
     cnpjInputs.forEach(input => {
         IMask(input, {
             mask: '00.000.000/0000-00'
-        })
-    })
+        });
+    });
 
     const cepInputs = container.querySelectorAll('#zipcode, #zipcodeClient');
     cepInputs.forEach(input => {
         IMask(input, {
             mask: '00000-000'
-        })
-    })
+        });
+    });
 }

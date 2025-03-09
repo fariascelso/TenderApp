af04db2adbbe1e6d28938f8fd6dd75d9f9d6955c
Celso Farias <c__farias@outlook.com>
2025-03-09 12:30:00 -0300

    feat: add navigation

---
+++ public/scripts/navigation.js
@@ -0,0 +1,8 @@
+// scripts/navigation.js
+export function navigateToListarOrcamentos() {
+    window.location.href = "pages/listorders.html";
+}
+
+export function navigateToCriarOrcamento() {
+    window.location.href = "index.html";
+}
